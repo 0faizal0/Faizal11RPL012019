f78f237f1d5eea3fe1bc81d68b97a56d9810c50f
0faizal0 <faizal.10rpl1.2019@gmail.com>
2020-08-14 17:33:42 +0700

    add SP dan Menu

---
+++ app/src/main/java/com/example/faizal11rpl012019/ControlClass.java
@@ -0,0 +1,12 @@
+package com.example.faizal11rpl012019;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ControlClass extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
