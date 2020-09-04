06a0019378136cdc492fa863fbb35190bd7f1de3
0faizal0 <faizal.10rpl1.2019@gmail.com>
2020-09-04 15:46:57 +0700

    addDataOnline

--- app/src/main/java/com/example/faizal11rpl012019/ListData.java
+++ app/src/main/java/com/example/faizal11rpl012019/ListData.java
@@ -5,7 +5,17 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageView;
+
+import com.androidnetworking.AndroidNetworking;
+import com.androidnetworking.common.Priority;
+import com.androidnetworking.error.ANError;
+import com.androidnetworking.interfaces.JSONObjectRequestListener;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 
@@ -21,18 +31,19 @@ public class ListData extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_data);
         recyclerView = (RecyclerView) findViewById(R.id.rvdata);
-        addData();
+        //addData();
+        addDataOnline();
     }
 
     void addData() {
         //offline, isi data offline dulu
         DataArrayList = new ArrayList<>();
         Model data1 = new Model();
-        data1.setOriginal_title("Tenet (2020)");
+        data1.setOriginal_title("Judul Film");
         data1.setVote_count(100);
         data1.setPoster_path("https://image.tmdb.org/t/p/w500/k68nPLbIST6NP96JmTxmZijEvCA.jpg");
         data1.setAdult(false);
-        data1.setOverview(" Action, Sci-Fi ");
+        data1.setOverview("Deskripsi film disini");
         data1.setRelease_date("01-01-2020");
         DataArrayList.add(data1);
 
@@ -53,7 +64,64 @@ public class ListData extends AppCompatActivity {
         recyclerView.setAdapter(adapter);
 
         //get data online
+    }
 
+    void addDataOnline(){
+        AndroidNetworking.get("https://api.themoviedb.org/3/movie/now_playing?api_key=4344c92b5de6a2d3648c385808b1601f&language=en-US")
+                .setTag("test")
+                .setPriority(Priority.LOW)
+                .build()
+                .getAsJSONObject(new JSONObjectRequestListener() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        // do anything with response
+                        Log.d("hasiljson", "onResponse: " + response.toString());
+                        //jika sudah berhasil debugm lanjutkan code dibawah ini
+                        DataArrayList = new ArrayList<>();
+                        Model modelku;
+                        try {
+                            Log.d("hasiljson", "onResponse: " + response.toString());
+                            JSONArray jsonArray = response.getJSONArray("results");
+                            Log.d("hasiljson2", "onResponse: " + jsonArray.toString());
+                            for (int i = 0; i < jsonArray.length(); i++) {
+                                modelku = new Model();
+                                JSONObject jsonObject = jsonArray.getJSONObject(i);
+                                modelku.setOriginal_title(jsonObject.getString("original_title"));
+                                modelku.setOverview(jsonObject.getString("overview"));
+                                modelku.setPoster_path("https://image.tmdb.org/t/p/w500"+jsonObject.getString("poster_path"));
+                                modelku.setAdult(jsonObject.getBoolean("adult"));
+                                modelku.setVote_count(jsonObject.getInt("vote_count"));
+                                modelku.setRelease_date(jsonObject.getString("release_date"));
+                                DataArrayList.add(modelku);
+                            }
 
+                            adapter = new DataAdapter(DataArrayList, new DataAdapter.Callback() {
+                                @Override
+                                public void onClick(int position) {
+
+                                }
+
+                                @Override
+                                public void test() {
+
+                                }
+                            });
+                            RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(ListData.this);
+                            recyclerView.setLayoutManager(layoutManager);
+                            recyclerView.setAdapter(adapter);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+
+                    @Override
+                    public void onError(ANError error) {
+                        // handle error
+                        Log.d("errorku", "onError errorCode : " + error.getErrorCode());
+                        Log.d("errorku", "onError errorBody : " + error.getErrorBody());
+                        Log.d("errorku", "onError errorDetail : " + error.getErrorDetail());
+                    }
+                });
     }
 }
